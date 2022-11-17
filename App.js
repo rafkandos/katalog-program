9d0860dcde92a41c85c9a98f0d8a98795a1ef664
raffli <rafflitok@gmail.com>
2022-11-17 18:15:17 +0700

    initial

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { FlatList, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Card } from 'react-native-paper';
+import styles from './styles';
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/algosigma/js-reactjs/master/homestays.json')
+    .then((response) => response.json())
+    .then((hasil) => setData(hasil))
+    .catch(error => console.log)
+  }, []);
+
+  const listHomestay = ({item}) => {
+    return (
+      <Card style={styles.cardUtama}>
+        <View style={styles.containerTitle}>
+          <Text style={styles.title}>{item.nama}</Text>
+        </View>
+        <View style={styles.containerImage}>
+          <ImageBackground style={styles.image}
+          source={{uri:item.fotoUrl}}>
+            <Text style={styles.price}>Rp. {item.harga} Rb</Text>
+          </ImageBackground>
+        </View>
+      </Card>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, padding: 24}}>
+      <FlatList
+      data = {data}
+      keyExtractor = {item => item.id}
+      renderItem = {listHomestay} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
